d37bb957ed55771c214699d7bbf6e16a250605ee
Lucas Taubert <thezorg@hotmail.com>
2013-06-03 19:54:36 +0000

    Dodgefail

--- src/WigglyPuff/WigglyPuff.java
+++ src/WigglyPuff/WigglyPuff.java
@@ -1,19 +1,27 @@
 package WigglyPuff;
 
-import robocode.HitByBulletEvent;
-import robocode.HitWallEvent;
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
+import java.awt.Color;
+import java.util.Random;
 
-import java.awt.*;
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
  * BestTest - a robot by (your name here)
  */
-public class WigglyPuff extends Robot
-{
+public class WigglyPuff extends AdvancedRobot {
+
+	private long then = System.currentTimeMillis();
+	private double findingClose = 0.5;
+	private double shootThresh = 200;
+	private double closest = Integer.MAX_VALUE;
+	private int moveDir = 1;
+	private int gunDir = 1;
+	private double otherEnergy = 100;
+	private Random rng = new Random();
+
 	/**
 	 * run: BestTest's default behavior
 	 */
@@ -23,51 +31,56 @@ public class WigglyPuff extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setColors(Color.red, Color.blue, Color.green); // body,gun,radar
+		setTurnGunRight(99999);
 
 		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(10);
-			turnGunRight(360);
-			back(10);
-			turnGunRight(360);
-		}
 	}
 
-  /*
-   * Returns true f within fireing distance*/
-  private boolean withingDistance(ScannedRobotEvent e) {
-    if (e.getDistance() < 300) {
-      return true;
-    }
-    return false;
-  }
-
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		if (withingDistance(e) && getEnergy() > 50) {
-		  fire(3);
-    } else {
-      fire(1);
-	  }
-  }
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
-	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if(findingClose > 0){
+			double leNice = e.getDistance();
+			if(leNice < closest){
+				closest = leNice;
+				if(leNice < shootThresh){
+					fire(3);
+				}
+			}
+			long now = System.currentTimeMillis();
+			findingClose-=(double)(now-then)/1000;
+			return;
+		}
+		
+		if(rng.nextInt(15) == 0){
+			findingClose = 0.5;
+		}
+		
+		// Funky movements
+		setTurnRight(e.getBearing() + 75 + (rng.nextInt(30))
+				* moveDir);
+
+		// If the bot has small energy drop,
+		// assume it fired
+		double changeInEnergy = otherEnergy - e.getEnergy();
+		// Shots leech 0-3 energy from yourself, monitor this!
+		if (changeInEnergy > 0 && changeInEnergy <= 3) {
+			// Dodge!
+			moveDir = -moveDir;
+			setAhead(99999 * moveDir);
+		}
+		// When a bot is spotted,
+		// sweep the gun and radar
+		gunDir = -gunDir;
+		setTurnGunRight(99999 * gunDir);
+
+		// Fire directly at target
+		if(e.getDistance() < shootThresh){
+			fire(3);
+		}
+		// Track the energy level
+		otherEnergy = e.getEnergy();
+	}
+
 }
